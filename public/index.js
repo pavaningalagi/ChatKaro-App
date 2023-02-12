25cf1cf618b41451c8467985f2b59f4cbacb995c
Pavan Ingalagi <ingalagipavan@gmail.com>
2023-02-12 15:19:10 +0530

    Update index.js

--- public/index.js
+++ public/index.js
@@ -4,7 +4,7 @@ const sendbtn = document.getElementById("sendbtn");
 const messageInp = document.getElementById("messageInp");
 const userdata = document.getElementById("userdata");
 
-const socket = io("http://localhost:8080/", { transports: ["websocket"] });
+const socket = io( { transports: ["websocket"] });
 
 
 let username = prompt(`Welcome to ChartKaro App! \n \n Please enter your username: `);
